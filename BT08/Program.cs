27846198f32a1812dd8b38a24ad3f709724da06a
Dũng Đặng Ngọc <dangngocdung12001@gmail.com>
2023-02-07 08:59:58 +0700

    Thêm comment

--- BT08/Program.cs
+++ BT08/Program.cs
@@ -5,11 +5,13 @@ namespace BT08
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
+        /// 
         [STAThread]
         static void Main()
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+            // Thêm comment
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
